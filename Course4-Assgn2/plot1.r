c1fd352ca5a1618301c62bc0b5a42777b6e0c986
Doug Luurs <dluurs15@gmail.com>
2015-02-22 19:16:35 -0500

    Inital Commit of Assignment 2 / Course 4

---
+++ Course4-Assgn2/plot1.r
@@ -0,0 +1,43 @@
+# Course 4 - Assignment 2 - Question 1.
+#
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
+#
+# Using the base plotting system, make a plot showing the total PM2.5 emission from
+# all sources for each of the years 1999, 2002, 2005, and 2008.
+#
+# Set to your data source directory
+
+workingdirectory <- "C:/Data/"
+
+setwd( workingdirectory )
+
+# Load datasets as needed
+# This data set will likely take a few seconds. Be patient!
+
+loaded <- (exists("NEI") && is.data.frame(get("NEI")))
+
+if (!loaded) {
+  print ("Loading NEI dataset .. Please stand by")
+  NEI <- readRDS("summarySCC_PM25.rds")
+}
+
+loaded <- (exists("SCC") && is.data.frame(get("SCC")))
+
+if (!loaded) {
+  print ("Loading SCC dataset .. Please stand by")
+  SCC <- readRDS("Source_Classification_Code.rds")
+}
+
+# Summarize Data
+
+print ("Aggregating Data Totals .. Please stand by")
+aggrTotals <- with(NEI, aggregate(Emissions, by = list(year), sum) )
+
+# Generate Plot
+
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+
+plot(aggrTotals, type = "b", pch = 18, col = "red", 
+     xlab = "Year", ylab = "Emissions", main = "United States PM2.5 Emissions")
+
+dev.off()
